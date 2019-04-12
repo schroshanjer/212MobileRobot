58b37cde1250f8597834c08c7e3dd07f2e3d85d0
sjxue <sjxue@mit.edu>
2019-04-12 11:53:47 -0400

    tele op

---
+++ catkin_ws/src/me212bot/scripts/keyboard_test.py
@@ -0,0 +1,14 @@
+import struct
+mou = open( "/dev/input/mice", "rb" );
+def m_event():
+    m = mou.read(3)
+    b = ord( m[0] )
+    bl = b & 0x1
+    bm = ( b & 0x4 ) > 0
+    br = ( b & 0x2 ) > 0
+    x,y = struct.unpack( "bb", m[1:] )
+    print "Left:%d, Middle: %d, Right: %d, x: %d, y: %d\n" % (bl,bm,br, x, y)
+
+while( 1 ):
+    m_event()
+    mou.close();
