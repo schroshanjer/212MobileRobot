7f715f9e30c351ca474658a658012578baea712f
sjxue <sjxue@mit.edu>
2019-04-20 19:30:52 -0400

    path planning

--- catkin_ws/src/me212bot/scripts/path_planning_node.py
+++ catkin_ws/src/me212bot/scripts/path_planning_node.py
@@ -76,7 +76,7 @@ class PathPlanningNode:
         self.vel_desired=0.1
         debug=None
         if self.laser_data:
-            alpha,debug=find_direction(self.laser_data,margin=0.25,stop_margin=0.3)
+            alpha,debug=find_direction(self.laser_data,margin=0.23,stop_margin=0.23)
             if alpha is None:
                 desiredWV_R,desiredWV_L=(0,0)
             else:
