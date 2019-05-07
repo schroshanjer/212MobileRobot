ee2526de7434fb06176a3e95aae822879d6da404
sjxue <sjxue@mit.edu>
2019-05-06 20:04:39 -0400

    localization node

--- catkin_ws/src/me212bot/scripts/localization.py
+++ catkin_ws/src/me212bot/scripts/localization.py
@@ -31,17 +31,19 @@ class State(object):
         self.tag_num=2
         #self.tag_pose=np.array([0,0,-np.pi])
         self.tag_data=[None]*self.tag_num
-        self.tag_measure_time=None
+        self.tag_time=None
+        self.last_tag_time=None
     def clear_measure(self):
+        self.last_tag_time=self.tag_time
         self.encoder_data=[]
         self.tag_data=[None]*self.tag_num
-        self.tag_measure_time=None
+        self.tag_time=None
 
 state=State()
 
 
 def april_tag_callback(data,tag_id):
-    state.tag_measure_time=data.header.stamp.to_sec()
+    state.tag_time=data.header.stamp.to_sec()
     data=pose2poselist(data.pose)
     x,y=data[0:2]
     theta=tfm.euler_from_quaternion(data[3:7]) [2]
@@ -60,7 +62,7 @@ def Localization():
         rospy.sleep(1.)
         #print state.encoder_data
         print state.tag_data
-        print state.tag_measure_time
+        print state.tag_time
         state.clear_measure()
         pass
 
